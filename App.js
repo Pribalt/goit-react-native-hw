16846180b247503496b95bb495d4fc1634e5db83
Pribalt <vitalisvitalik@gmail.com>
2023-07-18 14:47:41 +0300

    change RegistrationScreen.js

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { StyleSheet, View, ImageBackground } from "react-native";
+import { StyleSheet, View } from "react-native";
 import RegistrationScreen from "./Screens/RegistrationScreen";
 import LoginScreen from "./Screens/LoginScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={require("./assets/images/photo-bg.jpg")}
-        resizeMode="cover"
-        style={styles.image}
-      ></ImageBackground>
       <RegistrationScreen />
       {/* <LoginScreen /> */}
     </View>
@@ -21,11 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-    flex: 1,
-    justifyContent: "center",
   },
 });
--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,20 +1,76 @@
 import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  ImageBackground,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from "react-native";
 
 export default function RegistrationScreen() {
   return (
-    <View>
-      <TextInput style={styles.input} />
-      <Text>Registration</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.image}
+        source={require("../assets/images/photo-bg.jpg")}
+      >
+        <View style={styles.form}>
+          <Text style={styles.formTitle}>Registration</Text>
+          <TextInput style={styles.input} placeholder="Login" />
+          <TextInput style={styles.input} placeholder="Email address" />
+          <TextInput
+            style={styles.input}
+            secureTextEntry={true}
+            placeholder="Password"
+          />
+          <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
+            <Text style={styles.btnTitle}>Register</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+  },
+  form: {
+    marginHorizontal: 16,
+  },
+  formTitle: {
+    textAlign: "center",
+    fontSize: 30,
+    color: "#212121",
+    marginBottom: 33,
+  },
   input: {
-    height: 40,
-    margin: 12,
     borderWidth: 1,
-    padding: 10,
+    borderRadius: 8,
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
+    height: 50,
+    padding: 16,
+    marginBottom: 16,
+  },
+  btn: {
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    height: 50,
+    marginTop: 27,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnTitle: {
+    color: "#FFFFFF",
+    fontSize: 16,
   },
 });
