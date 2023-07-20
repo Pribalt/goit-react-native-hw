9ef87628a733fc679255a04855a74d581b399585
Pribalt <vitalisvitalik@gmail.com>
2023-07-20 21:33:22 +0300

    change RegistrationScreen.js

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -6,40 +6,72 @@ import {
   TextInput,
   View,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 
 export default function RegistrationScreen() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  console.log(isShowKeyboard);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.image}
-        source={require("../assets/images/photo-bg.jpg")}
-      >
-        <View style={styles.form}>
-          <Text style={styles.formTitle}>Registration</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Login"
-            placeholderTextColor={"#BDBDBD"}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Email address"
-            placeholderTextColor={"#BDBDBD"}
-          />
-          <TextInput
-            style={styles.input}
-            secureTextEntry={true}
-            placeholder="Password"
-            placeholderTextColor={"#BDBDBD"}
-          />
-          <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
-            <Text style={styles.btnTitle}>Register</Text>
-          </TouchableOpacity>
-          <Text style={styles.linkText}>Already have an account? Sign in</Text>
-        </View>
-      </ImageBackground>
-    </View>
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.image}
+          source={require("../assets/images/photo-bg.jpg")}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+          >
+            <View
+              style={{
+                ...styles.form,
+                marginBottom: isShowKeyboard ? -174 : 0,
+              }}
+            >
+              <Text style={styles.formTitle}>Registration</Text>
+              <TextInput
+                style={{ ...styles.input, marginBottom: 16 }}
+                placeholder="Login"
+                placeholderTextColor={"#BDBDBD"}
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+              <TextInput
+                style={{ ...styles.input, marginBottom: 16 }}
+                placeholder="Email address"
+                placeholderTextColor={"#BDBDBD"}
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+              <TextInput
+                style={{ ...styles.input, marginBottom: 43 }}
+                secureTextEntry={true}
+                placeholder="Password"
+                placeholderTextColor={"#BDBDBD"}
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+              <TouchableOpacity
+                activeOpacity={0.7}
+                style={styles.btn}
+                onPress={keyboardHide}
+              >
+                <Text style={styles.btnTitle}>Register</Text>
+              </TouchableOpacity>
+              <Text style={styles.linkText}>
+                Already have an account? Sign in
+              </Text>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -73,7 +105,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#F6F6F6",
     height: 50,
     padding: 16,
-    marginBottom: 16,
     fontSize: 16,
     lineHeight: 19,
   },
@@ -81,7 +112,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#FF6C00",
     borderRadius: 100,
     height: 50,
-    marginTop: 27,
+
     justifyContent: "center",
     alignItems: "center",
     marginBottom: 16,
